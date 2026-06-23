13dc371f520f2ffead437db141b97ab5315747c2
ShenrickGuzman <shenrickguzman07@gmail.com>
2026-06-24 07:29:22 +0800

    Remove fileUrls, fix separator

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -27,8 +27,7 @@ export default function Order() {
 
     const fd = new FormData(e.target)
     const total = Number(fd.get('copies')) * 3
-    const fileNames = uploaded.map((f) => `${f.original_filename}.${f.format}`).join(', ')
-    const fileUrls = uploaded.map((f) => f.secure_url).join(', ')
+    const fileNames = uploaded.map((f) => `${f.original_filename}.${f.format}: ${f.secure_url}`).join(', ')
 
     const params = {
       name: fd.get('name'),
@@ -37,7 +36,6 @@ export default function Order() {
       total: String(total),
       details: fd.get('details'),
       fileNames,
-      fileUrls,
     }
 
     try {
