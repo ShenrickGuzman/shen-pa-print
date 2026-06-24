8fd2f3b1821783aefd6b88c7c8dc98be388d66f4
ShenrickGuzman <shenrickguzman07@gmail.com>
2026-06-24 20:48:02 +0800

    Use explicit SMTP config for Gmail

--- api/sendmail.js
+++ api/sendmail.js
@@ -6,7 +6,9 @@ export default async function handler(req, res) {
   const { ownerEmail, customerEmail, subject, html, customerSubject, customerHtml } = req.body
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASS },
   })
 
